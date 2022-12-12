53bdeddfd9bb957d461850289394e52f1b1cde4a
gabriel-cardinaux <gabriel.cardinaux@gmail.com>
2022-12-12 20:56:14 -0300

    Log
    Changes; shopping cart

--- main.js
+++ main.js
@@ -1,20 +1,112 @@
 //alert("Chequeando linkeado para live server")
 
-function convertir () {
-    let valor = parseInt (document.getElementById ("valor").value);
-    let resultado = 0;
-    let dolar = 320.00;
-    let euro = 172.27;
+// function convertir () {
+//     let valor = parseInt (document.getElementById ("valor").value);
+//     let resultado = 0;
+//     let dolar = 320.00;
+//     let euro = 172.27;
 
-    if (document.getElementById("uno").checked){
-        resultado = valor / dolar;
-        alert("El cambio es: $" + resultado.toFixed(2));
-    }
-    else if (document.getElementById("dos").checked){
-        resultado = valor / euro;
-        alert("El cambio es: €" + resultado.toFixed(2));
-    }
-    else {
-        alert("Introduzca el valor a cotizar")
-    }
+//     if (document.getElementById("uno").checked){
+//         resultado = valor / dolar;
+//         alert("El cambio es: $" + resultado.toFixed(2));
+//     }
+//     else if (document.getElementById("dos").checked){
+//         resultado = valor / euro;
+//         alert("El cambio es: €" + resultado.toFixed(2));
+//     }
+//     else {
+//         alert("Introduzca el valor a cotizar")
+//     }
+// }
+
+// Carrito de compras
+// Arrays con lista de productos
+const products = [
+    {name: "English book starter", price: 2000},
+    {name: "English book intermediate", price: 2500},
+    {name: "English book advanced", price: 3000},
+    {name: "French book starter", price: 2500},
+    {name: "French book intermediate", price: 3000},
+    {name: "French book advanced", price: 3500},
+    {name: "Portuguese book starter", price: 1500},
+    {name: "Portuguese book intermediate", price: 2500},
+    {name: "Portuguese book advanced", price: 3000},
+
+];
+// Carrito función
+let shoppingCart = []
+
+let selection = prompt("Do you want to buy any product available? Yes or No?")
+//Bucle inicial
+while(selection != "Yes" && selection != "No"){
+    alert("Please select Yes or No")
+    selection = prompt("Yes or No?")
 }
+if(selection == "Yes"){
+    alert("Products available")
+    let listOfProducts = products.map((product) => product.name + "-" + "$" + product.price);
+    alert(listOfProducts.join(" - "))
+}
+else if(selection == "No"){
+    alert("Thanks for your visit")
+}
+//Bucle difiero de No
+while(selection != "No"){
+    let product = prompt("Select an item to add to your shopping cart")
+    let price = 0
+//Casos por producto con switch
+    if (product == "English book starter" || product == "English book intermediate" || product == "English book advanced" ||
+        product == "French book starter" || product == "French book intermediate" || product == "French book advanced" ||
+        product == "Portuguese book starter" || product == "Portuguese book intermediate" || product == "Portuguese book advanced"){
+            switch(product){
+                case "English book starter":
+                price = 2000;
+                break;
+                case "English book intermediate":
+                price = 2500;
+                break;
+                case "English book advanced":
+                price = 3000;
+                break;
+                case "French book starter":
+                price = 2500;
+                break;
+                case "French book intermediate":
+                price = 3000;
+                break;
+                case "French book advanced":
+                price = 3500;
+                break;
+                case "Portuguese book starter":
+                price = 1500;
+                break;
+                case "Portuguese book intermediate":
+                price = 2500;
+                break;
+                case "Portuguese book advanced":
+                price = 3000;
+                break;
+            }
+//Cantidad
+            let quantity = parseInt(prompt("How many items do you want to purchase?"))
+
+            shoppingCart.push({product, quantity, price})
+            console.log(shoppingCart)
+        }
+        else {
+            alert("Product unavailable")
+        }
+        selection = prompt("Do you want to add any other product")
+//Cierre de bucle     
+        while(selection == "No"){
+            alert("Thanks for your purchase")
+            shoppingCart.forEach((shoppingCartTotal) =>{
+                alert(`product: ${shoppingCartTotal.product}, quantity: ${shoppingCartTotal.quantity}, Total to pay ${shoppingCartTotal.quantity * shoppingCartTotal.price}`)
+            })
+            break;
+        }
+}
+// Metodo reduce
+const total = shoppingCart.reduce((acc, the) => acc + the.price * the.quantity, 0)
+alert(`Total to pay: ${total}`)
+
